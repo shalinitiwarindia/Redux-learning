6579fbec3923e78bbbb0d8caa7085cedecc5630d
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-10-09 21:23:44 +0530

    Update Counter.jsx

--- day-2/src/Component/Counter.jsx
+++ day-2/src/Component/Counter.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import { addCounter,reduceCounter } from "../Redux/action";
+import { addCounter, reduceCounter } from "../Redux/action";
+import { AppContextProvider } from "../Redux/AppContextProvider";
 import { store } from "../Redux/store";
+import { AppContext } from "../Redux/AppContextProvider";
 
 const Counter = () => {
-    const [state,setState] = React.useState(0);
-    const {counter} = store.getState();
-    const {dispatch} = store;
+  const [state, setState] = React.useState(0);
+  const { dispatch, getState } = React.useContext(AppContext);
+  const { counter } = getState();
 
-    const forceUpdate = () => setState(state+1);
+  const forceUpdate = () => setState(state + 1);
 
-    const handleAdd = ()=>{
-        dispatch(addCounter(1));
-        forceUpdate();
-    };
-    const handleReduce = () => {
-        dispatch(reduceCounter(1));
-        forceUpdate();
-    };
-    return (
-        <>
-        <h1>Counter: {counter}</h1>
-        <button onClick={handleAdd}>ADD</button>
-        <button onClick={handleReduce}>REDUCE</button>
-        </>
-    );
+  const handleAdd = () => {
+    dispatch(addCounter(1));
+    forceUpdate();
+  };
+  const handleReduce = () => {
+    dispatch(reduceCounter(1));
+    forceUpdate();
+  };
+  return (
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={handleAdd}>ADD</button>
+      <button onClick={handleReduce}>REDUCE</button>
+    </>
+  );
 };
 
-export {Counter};
+export { Counter };
