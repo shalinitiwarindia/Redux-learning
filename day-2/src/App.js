c4a46155fe8aed0762bf25fb4a9895358005e6df
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-10-09 21:26:31 +0530

    Update App.js

--- day-2/src/App.js
+++ day-2/src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
+import { Counter } from "./Component/Counter";
+import { AppContext } from "./Redux/AppContextProvider";
 import "./styles.css";
 
 export default function App() {
+  const { getState } = React.useContext(AppContext);
+  const { counter } = getState();
   return (
     <div className="App">
-      <h1>Counter</h1>;
+      <Counter />
+      <div>{counter}</div>
     </div>
   );
 }
