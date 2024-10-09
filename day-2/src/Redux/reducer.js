bef6a88b9bdd9415e3db6889a51d16194b7a41ff
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-10-09 21:25:32 +0530

    Update reducer.js

--- day-2/src/Redux/reducer.js
+++ day-2/src/Redux/reducer.js
@@ -1,25 +1,24 @@
 import { ADD_COUNTER, REDUCE_COUNTER } from "./actionTypes";
 
-const initState={
-    counter:1
+const initState = {
+  counter: 1,
 };
 
-const reducer = ((state = initState), { type, payload })=>{
-    switch (type) {
-        case ADD_COUNTER:
-            return {
-                ...state,
-                counter:state.counter + payload
-            };
-        case REDUCE_COUNTER:
-            return {
-                ...state,
-                counter:state.counter-payload
-            };
-        default:
-            return state;
-    }
+const reducer = (state = initState, { type, payload }) => {
+  switch (type) {
+    case ADD_COUNTER:
+      return {
+        ...state,
+        counter: state.counter + payload,
+      };
+    case REDUCE_COUNTER:
+      return {
+        ...state,
+        counter: state.counter - payload,
+      };
+    default:
+      return state;
+  }
 };
 
-export {reducer};
-
+export { reducer };
