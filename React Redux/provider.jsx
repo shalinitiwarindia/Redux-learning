2a6d608f504692121780971d535e303e60cabe00
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-10-13 16:16:40 +0530

    Create provider.jsx

---
+++ React Redux/provider.jsx
@@ -0,0 +1,10 @@
+import { Provider } from 'react-redux'
+import store from './store'
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+  <Provider store={store}> //similar to context API
+    <App />
+  </Provider>,
+  rootElement
+)
