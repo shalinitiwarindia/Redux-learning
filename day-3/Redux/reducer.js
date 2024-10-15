4a1b8baea2b58359549d8dc83c0651ea545d12dc
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-10-15 21:39:17 +0530

    Create reducer.js

---
+++ day-3/Redux/reducer.js
@@ -0,0 +1,9 @@
+import { ADD_COUNT } from "./action"
+
+
+export const counterReducer=(store,{type,payload}) => {
+    switch (type) {
+        case ADD_COUNT:
+            return {...store,counter:store.counter}
+    }
+}
