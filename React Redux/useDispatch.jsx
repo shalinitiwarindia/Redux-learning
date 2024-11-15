3c83e8098c622982a162f57e737175e3c7a4d27a
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-15 09:14:52 +0530

    Update useDispatch.jsx

--- React Redux/useDispatch.jsx
+++ React Redux/useDispatch.jsx
@@ -1,1 +1,31 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+
+// action type (actionTypes.js)
+const INCREMENT_COUNTER = "INCREMENT_COUNTER";
+
+// action creator (action.js)
+const incrementCounter = () => {
+    return { 
+        type: INCREMENT_COUNTER
+    }
+}
+
+export const CounterComponent = ({ value }) => {
+  const dispatch = useDispatch()
+
+  const handleIncrementCounter = () => {
+      const action = incrementCounter()
+      dispatch( action )
+  }
+
+  return (
+    <div>
+      <span>{value}</span>
+      <button onClick={handleIncrementCounter}>
+        Increment counter
+      </button>
+    </div>
+  )
+}
 
