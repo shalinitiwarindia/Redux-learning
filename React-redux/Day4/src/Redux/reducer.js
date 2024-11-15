bef0c4bd76444c4f68ad060e6a67dff391eb6435
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-15 09:12:17 +0530

    Update reducer.js

--- React-redux/Day4/src/Redux/reducer.js
+++ React-redux/Day4/src/Redux/reducer.js
@@ -1,13 +1,1 @@
-import { ADD_COUNT, ADD_TODO } from "./action";
 
-export const counterReducer = (store,{type,payload})=>{
-    switch (type) {
-        case ADD_COUNT:
-            return {...store, counter : store.counter + payload};
-
-            case ADD_TODO:
-                return {...store,todos:[...store.todos,payload]};
-        default:
-            return store;
-    }
-};
