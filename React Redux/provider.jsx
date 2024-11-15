9c7fbd340f0b0b6ba53abd6441c41200de277a7c
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-15 09:14:07 +0530

    Update provider.jsx

--- React Redux/provider.jsx
+++ React Redux/provider.jsx
@@ -1,10 +1,1 @@
-import { Provider } from 'react-redux'
-import store from './store'
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(
-  <Provider store={store}> //similar to context API
-    <App />
-  </Provider>,
-  rootElement
-)
