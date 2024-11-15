9dc1bd2ffa4a02c199e7b4093b01084632bcf94c
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-15 09:13:26 +0530

    Update Counter.jsx

--- React-redux/Day4/src/component/Counter.jsx
+++ React-redux/Day4/src/component/Counter.jsx
@@ -1,1 +1,23 @@
 
+
+import { useDispatch, useSelector } from 'react-redux';
+import { addCount } from '../Redux/action';
+
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+const counter = useSelector((store)=> store.counter);
+
+return (
+    <div className='App'>
+     <h3>Counter: {counter}</h3>
+     <button 
+     onClick={()=>{
+     dispatch(addCount(1));
+     }}
+     >Add 1</button>
+
+     <hr/>
+    </div>
+  )
+}
