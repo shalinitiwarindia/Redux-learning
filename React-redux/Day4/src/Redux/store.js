3a7fad126dedaf9dabcf3052000f56b63814ba3d
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-15 09:12:53 +0530

    Update store.js

--- React-redux/Day4/src/Redux/store.js
+++ React-redux/Day4/src/Redux/store.js
@@ -1,1 +1,11 @@
 
+import { legacy_createStore as createStore } from "redux";
+
+import { counterReducer } from "./reducer";
+
+export const store = createStore(counterReducer,{counter:0, todos:[]});
+// store.subscribe(()=>{
+//     console.log("Subscribe:",store.getState());
+// })
+
+
