7da080ce93330ce2ace42d0aa6fc90eee39d3883
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-16 20:59:39 +0530

    Create store.js

---
+++ Combine Reducer/src/Redux/store.js
@@ -0,0 +1,16 @@
+
+import { legacy_createStore as createStore, combineReducers } from "redux";
+
+import { counterReducer } from "./Counter/reducer";
+import { todosReducer } from "./Todos/reducer";
+const rootReducer = combineReducers({
+    counter: counterReducer,
+    todos: todosReducer,
+});
+export const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    
+);
+
+console.log("State",store.getState());
